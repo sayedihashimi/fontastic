5cd133c7412338ecfedd99c12afdb911f1ed5dc5
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2013-04-10 21:26:44 -0700

    starting webapi

---
+++ EdgefontsWeb/Controllers/api/BaseFontController.cs
@@ -0,0 +1,23 @@
+﻿namespace EdgefontsWeb.Controllers.api
+{
+    using Edgefonts;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Web;
+    using System.Web.Http;
+
+    public abstract class BaseFontController : ApiController
+    {
+        protected virtual FontManager NewFontManager() {
+            // TODO: Could pass this in via IOC            
+
+            // System.Web.HttpContext.Current
+            HttpContextBase ctxBase = new System.Web.HttpContextWrapper(System.Web.HttpContext.Current);
+
+            return new FontManager(ctxBase, new FontInfoParser());
+        }
+    }
+}
---
+++ EdgefontsWeb/Controllers/api/FontInfoController.cs
@@ -0,0 +1,18 @@
+﻿namespace EdgefontsWeb.Controllers.api {
+    using Edgefonts;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http;
+
+    public class FontInfoController : BaseFontController {
+        public HttpResponseMessage Get() {
+            // this will return all the fonts
+            IList<IFontInfo>allFonts = this.NewFontManager().GetFonts();
+
+            return Request.CreateResponse<IList<IFontInfo>>(HttpStatusCode.OK, allFonts);
+        }
+    }
+}
